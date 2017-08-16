4d62fc9f01159520c4a2ec65fcefc3769d72c06a
lingjiawang511 <398677675@qq.com>
2017-08-16 09:51:31 +0800

    update the QQ online service

--- data/module/modulescache.php
+++ data/module/modulescache.php
@@ -4,6 +4,7 @@ $GLOBALS['allmodules']['72ffa6fabe3c236f9238a2b281bc0f93']='72ffa6fabe3c236f9238
 $GLOBALS['allmodules']['fcd00dbb5a51d65aff6c248d221d8bcd']='fcd00dbb5a51d65aff6c248d221d8bcd.xml';
 $GLOBALS['allmodules']['1f35620fb42d452fa2bdc1dee1690f92']='1f35620fb42d452fa2bdc1dee1690f92.xml';
 $GLOBALS['allmodules']['606c658db048ea7328ffe1c7ae2a732f']='606c658db048ea7328ffe1c7ae2a732f.xml';
+$GLOBALS['allmodules']['0a7bea5dbe571d35def883cbec796437']='0a7bea5dbe571d35def883cbec796437.xml';
 $GLOBALS['allmodules']['b437d85a7a7bc778c9c79b5ec36ab9aa']='b437d85a7a7bc778c9c79b5ec36ab9aa.xml';
 $GLOBALS['allmodules']['81323e93cd52ecce9f175b6aa46f5cfd']='81323e93cd52ecce9f175b6aa46f5cfd.xml';
 $GLOBALS['allmodules']['0cce60bc0238aa03804682c801584991']='0cce60bc0238aa03804682c801584991.xml';
