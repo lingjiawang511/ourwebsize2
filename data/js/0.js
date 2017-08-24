27cb891a10854a57f9e582bf2213bcbc35e57939
lingjiawang511 <398677675@qq.com>
2017-08-24 16:56:59 +0800

    fix the online message with websize default type

---
+++ data/js/0.js
@@ -0,0 +1,11 @@
+<!--
+document.write("·<a href='/canshu/High_meter.html'>高级液晶显示称重仪</a><br>");
+document.write("·<a href='/canshu/smg_meter.html'>数码管显示称重手持</a><br>");
+document.write("·<a href='/plus/view.php?aid=1'>动态电子镇流器</a><br>");
+document.write("·<a href='/canshu/11.html'>测试文档2</a><br>");
+document.write("·<a href='/canshu/10.html'>测试文档</a><br>");
+document.write("·<a href='/product/kaiguan/9.html'>无开关智能灯2</a><br>");
+document.write("·<a href='/product/led/wulian/8.html'>镇流器</a><br>");
+document.write("·<a href='/product/hid/7.html'>hid镇流器2</a><br>");
+
+-->
